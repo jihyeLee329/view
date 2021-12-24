7ff50fe9b616a7b3c500f26d4de73601c83355d7
이지혜 <jihyelee.329@gmail.com>
2021-12-24 17:53:28 +0900

    style 주석 수정

--- account/js/common.js
+++ account/js/common.js
@@ -1,7 +1,3 @@
-//swiper 
-// var swiper = new Swiper(".container", {});
-
-
 // 네비게이션 메뉴 클릭시, on 클래스 추가
 const nav_lis = document.querySelectorAll('nav li');
 for (n = 0; n < nav_lis.length; n++) {
@@ -15,163 +11,77 @@ for (n = 0; n < nav_lis.length; n++) {
 
 
 const main_sliders = document.querySelectorAll('.main_slider');
+//save_container의 각각 save의 graph width 조정
+let grh_percents = document.querySelector('.save_container');
+grh_percents = grh_percents.querySelectorAll('.save')
+for (p = 0; p < grh_percents.length; p++) {
+    const grph_percent = grh_percents[p].querySelector('.save_graph');
+    let grph_percent_data = Number(grph_percent.dataset.percent);
 
-// for (m = 0; m < main_sliders.length; m++) {
-    
+    let grph_percent_plt = parseInt(grph_percent_data / 20);
 
-    //save_container의 각각 save의 graph width 조정
-    let grh_percents = document.querySelector('.save_container');
-    grh_percents = grh_percents.querySelectorAll('.save')
-    for (p = 0; p < grh_percents.length; p++) {
-        const grph_percent = grh_percents[p].querySelector('.save_graph');
-        let grph_percent_data = Number(grph_percent.dataset.percent);
-
-        let grph_percent_plt = parseInt(grph_percent_data / 20);
-
-        switch (grph_percent_plt) {
-            case 0:
-                grph_percent.style.background = "#FF5F00";
-                break;
-            case 1:
-                grph_percent.style.background = "#FEB700";
-                break;
-            case 2:
-                grph_percent.style.background = "#55ACEE";
-                break;
-            case 3:
-                grph_percent.style.background = "#0A73C3";
-                break;
-            default:
-                grph_percent.style.background = "#005F59";
-        }
-        grph_percent.style.width = `${grph_percent_data}%`;
+    switch (grph_percent_plt) {
+        case 0:
+            grph_percent.style.background = "#FF5F00";
+            break;
+        case 1:
+            grph_percent.style.background = "#FEB700";
+            break;
+        case 2:
+            grph_percent.style.background = "#55ACEE";
+            break;
+        case 3:
+            grph_percent.style.background = "#0A73C3";
+            break;
+        default:
+            grph_percent.style.background = "#005F59";
     }
+    grph_percent.style.width = `${grph_percent_data}%`;
+}
 
 
+// 이체화면에서 취소 클릭햇을 때 removeClass'show'
+document.querySelector('.close_btn').addEventListener('click', function () {
+    target.classList.remove('show')
+});
 
-    //이체 버튼 클릭했을 때 addClass 'show'
-    const target = document.querySelector(".transfer_container");
-    document.querySelector('.transfer_btn').addEventListener('click', function () {
-        
-        
-        let bottom = -100;
-        target.classList.add('show');
-        // function move() {
-        //     bottom += 1;
-        //     target.style.bottom = `${bottom}%`;
-
-        //     if (bottom == 0) {
-        //         cancelAnimationFrame(move())
-        //     } else{
-        //         requestAnimationFrame(move())
-              
-                
-        //     }
-        // }
-
-        // window.requestAnimationFrame(move);
-
-
+// 이체리스트 즐겨찾기 체크, 해제 
+const favorites = target.querySelectorAll('.transfer_list');
+for (let s = 0; s < favorites.length; s++) {
+    favorites[s].querySelector('.favorite_btn').addEventListener('click', function () {
+        this.classList.toggle('chk');
     });
-
-    // 이체화면에서 취소 클릭햇을 때 removeClass'show'
-    document.querySelector('.close_btn').addEventListener('click',function(){
-        target.classList.remove('show')
-    });
-
-    const favorites = target.querySelectorAll('.transfer_list');
-    for(let s=0; s < favorites.length; s ++){
-        favorites[s].querySelector('.favorite_btn').addEventListener('click',function(){
-           this.classList.toggle('chk');
-        });
-    }
+}
 
 
-    //kslider height 조정
-    const kslider_btn = document.querySelector('.kslider_btn');
-    const kslider = document.querySelector('.kslider');
-    kslider_btn.addEventListener('click', function () {
-        const Btn_on = kslider.classList.contains('click');
-        kslider.classList.toggle('click');
-    });
-
- 
-    //목표금액 range바 gradient 삽입
-    const input_range = document.querySelector('#account_target');
-    //처음 설정한 value 값 만큼 gradient 넣어주기
-    const current_val = parseInt((Number(input_range.value)/Number(input_range.getAttribute('max')))*100);
-    input_range.style.background = 'linear-gradient(to right, #FFDB4C 0%, #FFDB4C '+ current_val +'%, #d5d4d3 ' + current_val + '%, #d5d4d3 100%)';
-    //range input 클릭했을 때, 실시간으로 value 값 가져와 max금액/value 값 계산하여 gradient 주기
-    input_range.addEventListener('input', function(){
-        
-        const this_val = parseInt((Number(input_range.value)/Number(input_range.getAttribute('max')))*100);
-        this.style.background = 'linear-gradient(to right, #FFDB4C 0%, #FFDB4C '+ this_val +'%, #d5d4d3 ' + this_val + '%, #d5d4d3 100%)';
-    });
+//kslider height 조정
+const kslider_btn = document.querySelector('.kslider_btn');
+const kslider = document.querySelector('.kslider');
+kslider_btn.addEventListener('click', function () {
+    kslider.classList.toggle('click');
+});
 
 
-    // fetch('./account/json/bank.json')
-    //     .then(res => res.json())
-    //     .then(history_list => {
-    //         get_history(history_list)
-    //     });
+//목표금액 range바 gradient 삽입
+const input_range = document.querySelector('#account_target');
+//처음 설정한 value 값 만큼 gradient 넣어주기
+const current_val = parseInt((Number(input_range.value) / Number(input_range.getAttribute('max'))) * 100);
+input_range.style.background = 'linear-gradient(to right, #FFDB4C 0%, #FFDB4C ' + current_val + '%, #d5d4d3 ' + current_val + '%, #d5d4d3 100%)';
+//range input 클릭했을 때, 실시간으로 value 값 가져와 max금액/value 값 계산하여 gradient 주기
+input_range.addEventListener('input', function () {
 
-
-    
-    // function get_history(history_list) {
-    //     const history_ul = document.querySelector('.history');
-    //     const history_wrap = document.querySelector('.history_wrap');
-       
-    //     for (let h = 0; h < 100; h++) {
-    //         const history_li_wrap = history_ul.querySelector('li');
-    //         const history_name_wrap = history_li_wrap.querySelector('.name')
-    //         const history_price_wrap = history_li_wrap.querySelector('price')
-
-    //         const history_name = history_list[h].price;
-           
-    //         // console.log(history_list.bankList.length)
-
-    //     }
-    // }
-
-
-
-    // 가격 합산 찍어주기
-    //각각 day 마다 반복 실행
-    // let days = document.querySelector('.history_wrap');
-    // days = days.querySelectorAll('.day');
-    // for (let i = 0; i < days.length; i++) {
-
-    //     //각각 day 의 지출내역 반복 실행
-
-    //     let day_total = days[i].querySelector('.history');
-    //     day_total = day_total.querySelector('li')
-    //     let total_price = 0;
-    //     for (let t = 0; t < day_total.length; t++) {
-    //         //각각 day 의 지출에서 출금만 선택  
-    //         const withdraw_leng = day_total[t].querySelector('.withdraw')
-    //         let withdraw_price = unComma(withdraw_leng.textContent);
-    //         console.log(withdraw_price)
-
-    //         //출금 가격 합계 
-    //         total_price += Number(withdraw_price);
-    //         //각각 day의 total에 text 찍어주기
-    //         days[i].querySelector('.total').textContent = comma(total_price);
-    //     }
-    // }
-
-
-   
-
-// } //main_ slider 반복문
+    const this_val = parseInt((Number(input_range.value) / Number(input_range.getAttribute('max'))) * 100);
+    this.style.background = 'linear-gradient(to right, #FFDB4C 0%, #FFDB4C ' + this_val + '%, #d5d4d3 ' + this_val + '%, #d5d4d3 100%)';
+});
 
 
 //천자리마다 콤마 찍어주기
- function comma(price) {
+function comma(price) {
     return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
 //콤마 제거
- function unComma(pStr) {
+function unComma(pStr) {
     var strCheck = /\,/g;
     pStr = pStr.replace(strCheck, '');
     return pStr;
--- account/js/getData.js
+++ account/js/getData.js
@@ -1,4 +1,3 @@
-
 //bank.json 파일에서 데이터 가져오기
 fetch('https://jihyelee329.github.io/view/account/json/bank.json')
     .then(res => res.json())
@@ -18,7 +17,6 @@ function get_history(history_list) {
         return a > b ? -1 : a < b ? 1 : 0;
     });
 
-    // console.log(history_bank)
     const nDate = new Date('2021/10/4'); //오늘 날짜 임의로 설정
     const nDate_time = nDate.getTime(); //오늘 날짜를 시간으로 변경
 
@@ -33,22 +31,21 @@ function get_history(history_list) {
 
     let obj_num = 0; // 객체개수 개념 값. 
 
-
     //데이터의 수 만큼 반복 실행 
     for (h = 0; h < history_bank.length; h++) {
 
-        //1. 현재 날짜가 account_list.date 중에 있어 ? 
-        // 없어! == > account_list 구조랑 똑같은 객체를 만들어줘 
-        // account_list 현재 date, 랑 현재 info 넣어주면 돼 
-        // 있어! ==> 있으면, date 냅두고 info만 추가해줘 . 
+        //1. 현재 들어온 날짜가 account_list.date 중에 있어 ? 
+        // 없어! == > account_list 구조랑 똑같은 객체를 만들어줘, account_list 현재 date, 랑 현재 info 넣어주면 돼 
+        // 있어! ==> 있으면, date 냅두고 info만 추가해줘. 
+
+
         let numTrue = false; //임의의 false 값 할당
         let current_num = -1; //임의의 -1 값 할당
 
-        //현재 들어온 history_bank[h] 가 이전에 들어온 history_bank[h]의 date와 같은 것이 있는지 확인하기 위한 반복문 
 
+        //현재 들어온 history_bank[h] 가 이전에 들어온 history_bank[h]의 date와 같은 것이 있는지 확인하기 위한 반복문 
         //obj_num이 지금까지 만든 객체의 개수. 
         //현재 들어온 데이터랑 지금까지 만들어진 데이터랑 모두 비교해봐야해. 
-
         for (a = 0; a < obj_num; a++) {
             if (account_list[a].date == history_bank[h].date) {
                 //현재 데이터의 date와 이미 지나간 데이터의 date와 일치하는 값이 있다. 
@@ -63,59 +60,40 @@ function get_history(history_list) {
 
         //만약 현재 date 와 지난 date 가 같다면, 
         if (numTrue) {
-            //이미 만들어진 객체 account_list[n]번째의 list란 key 에 현재 반복 실행중에 들어온 history_bank[h] 값 넣기
+            //이미 만들어진 객체 account_list[n]번째의 list(key)에 현재 반복 실행중에 들어온 history_bank[h] 값 넣기
             account_list[current_num].list.push(history_bank[h]);
-
         } else {
 
-            // 번째 만들어주고,
             // account_list[arr_num]의 date에 내 현재 데이터 history_bank[h].date 넣어주고, info 넣어준다.
             account_list[obj_num] = {}; //빈 객체 선언.
 
-            //현재 들어온 데이터의 date 값이 새롭다! 그럼 새 객체 만들어 주기. ==> account_list[obj_num] . 여기서 obj_num은 반복 돌때마다 증가
-
+            //현재 들어온 데이터의 date 값이 새롭다! == 이전의 날짜와 비교해서 새로운 날짜다. 
+            //그럼 새 객체 만들어 주기. ==> account_list[obj_num] 여기서 obj_num은 반복 돌때마다 증가
             account_list[obj_num].date = history_bank[h].date;
             account_list[obj_num].list = [history_bank[h]]
 
-
             obj_num++; //현재 생성된 객체의 개수.  
         }
-
-
-
-        //    console.log( reHistory_list[hitoryList.date].date)
-        // console.log(reHistory_list[hitoryList.date])
-        // console.log(`${days_ago}일 전`)
-        // const history_name = history_bank[h].price;
-        // console.log(history_date)
     } //for문 끝
 
-
-    // console.log(account_list[obj_num])
-
-    // console.log(account_list)
-    for (let al = 0; al < obj_num; al++) {
-
-
-        
-    //dom 생성
     const history_wrap = document.querySelector('.history_wrap');
-    const day_elm = document.createElement('div');
-    day_elm.className = 'day';
-    const date_elm = document.createElement('p');
-    date_elm.className = 'date';
-    const date_total = document.createElement('span');
-    date_total.className = 'total';
-    const ul_elm = document.createElement('ul');
-    ul_elm.className = 'history';
-  
+    for (let al = 0; al < obj_num; al++) {
+        //dom 생성
 
+        const day_elm = document.createElement('div');
+        day_elm.className = 'day';
+        const date_elm = document.createElement('p');
+        date_elm.className = 'date';
+        const date_total = document.createElement('span');
+        date_total.className = 'total';
+        const ul_elm = document.createElement('ul');
+        ul_elm.className = 'history';
 
         //각각 새로 만든 날짜 n일 전인지 계산해주기
         let history_date = account_list[al].date
         history_date = new Date(history_date)
         history_date = history_date.getTime();
-       
+
         let days_ago = nDate_time - history_date; //오늘 날짜의 시간 - 데이터[h] 번째의 날짜 시간
         days_ago = days_ago / 1000 / 60 / 60 / 24 //시간을 다시 날짜로 변경
         days_ago = Math.abs(Number.parseInt(days_ago)); //절대값만 반환
@@ -133,54 +111,48 @@ function get_history(history_list) {
         }
         account_list[al].day = day
 
-
         //각각 n일 전 데이터 삽입
         date_elm.textContent = account_list[al].day;
         day_elm.append(date_elm);
         history_wrap.append(day_elm);
 
-        let total_add = 0;
-        //각각 li에 대한 for문
-        let list_len = account_list[al].list; 
-        for (let db = 0; db < list_len.length; db ++) {
-           const history_name = list_len[db].history;
-           const history_price =  Number(list_len[db].price);
-         console.log(history_price)
-           const history_income =  list_len[db].income;
-           const li_elm = document.createElement('li');
+        let total_add = 0; //가격합산을 위한 변수 선언
+        
+        //각 .day의 li 개수만큼 반복실행
+        let list_len = account_list[al].list;
+        for (let db = 0; db < list_len.length; db++) {
+            const history_name = list_len[db].history; //이름
+            const history_price = Number(list_len[db].price); //가격
+            const history_income = list_len[db].income; //입금 출금
+            const li_elm = document.createElement('li'); //li 생성
 
-           const name_elm = document.createElement('span');
-           name_elm.className ='name'
-           const price_elm = document.createElement('span');
-           price_elm.className ='price'
+            const name_elm = document.createElement('span');
+            name_elm.className = 'name'
+            const price_elm = document.createElement('span');
+            price_elm.className = 'price'
 
 
-            if(history_income == 'in'){
+            if (history_income == 'in') {
+                //입금일 경우 addClass deposit
                 price_elm.classList.add('desposit');
-             
-               
-            }else{
+            } else {
+                //출금일 경우 addClass withdraw, 가격 더해주기
                 price_elm.classList.add('withdraw');
                 total_add += history_price
             }
 
-           
+
             // total_add += history_price;
-           name_elm.textContent = history_name;
-           price_elm.textContent = comma(history_price);
-           li_elm.append(name_elm);
-           li_elm.append(price_elm);
-           ul_elm.append(li_elm);
-         
+            name_elm.textContent = history_name; //name 찍어주기
+            price_elm.textContent = comma(history_price); //가격 부분 찍어주고, comma 생성
+            li_elm.append(name_elm);
+            li_elm.append(price_elm);
+            ul_elm.append(li_elm);
+
         }
+
         day_elm.append(ul_elm);
         date_total.append(comma(total_add));
         date_elm.append(date_total)
-         
     }
-
-
-
-    
-
 }
